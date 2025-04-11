e1005454b595348536e2477d7fe714eba4e7e8c1
김준범 <kimjunbeom@gimjunbeom-ui-MacBookAir.local>
2025-04-11 13:38:20 +0900

    처음부터 전체 업로드

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-[#D8D8D8] h-screen p-4">
+      <h1 className="text-xl font-bold text-center">Stock Strategy App</h1>
+      {/* 차트와 UI가 이곳에 들어감 */}
+    </div>
+  );
+}
+
+export default App;
